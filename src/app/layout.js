225c1e5bf5e99448a187b1b43000c1bfa6bf62bf
jesusvite <nicolasvitejesus@gmail.com>
2023-09-25 13:46:02 -0600

    Agregue nuevas carpetas; common, components,dashboard. cada carpeta incluye contenido copiado de el github de GNDX para adelantar el proceso, trabaje en el componente de dashboard

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,16 @@
 
 import '@style/tailwind.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Shop | Store Online',
   description: 'Store ubicate in all the world',
 }
 
- function RootLayout({ children }) {
+export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
-}
-export default RootLayout
+     <html lang="en">
+       <body>{children}</body>
+     </html>
+   )
+ }
+ 
--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,10 @@ import { Fragment } from 'react';
 import { Popover, Transition } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
 
+
+
 const navigation = [
-  { name: 'Product', href: '#' },
+  { name: 'Product', href:'dashboard' },
   { name: 'Features', href: '#' },
   { name: 'Marketplace', href: '#' },
   { name: 'Company', href: '#' },
